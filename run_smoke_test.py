8dc36eb8d4dfc391f61888527c7541fcab542684
Vladislav <137306980+zeleniy21@users.noreply.github.com>
2023-07-19 13:46:13 +0300

    Update run_smoke_test.py

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
     description = "Run notion-py client smoke tests"
  parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
+     "--page", dest="page", help="page URL or ID", required=True, type=str
+    )
