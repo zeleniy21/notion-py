e1cb19c134bd8abed1bb825e018e02a9c5d74930
Vladislav <137306980+zeleniy21@users.noreply.github.com>
2023-08-10 19:25:59 +0300

    Update run_smoke_test.py

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -14,3 +14,5 @@ if __name__ == "__main__":
  args = parser.parse_args()
 
     token = args.token
+ if not token:
+        # if you don't want your terminal to be filled with messy token, then input your token_v2 at "NOTION_TOKEN"
