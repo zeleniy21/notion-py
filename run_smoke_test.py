fdf2f32df4033c31679681e9acd9f01ccd45c814
Vladislav <137306980+zeleniy21@users.noreply.github.com>
2023-10-13 18:21:03 +0300

    Update run_smoke_test.py

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -14,3 +14,8 @@ if __name__ == "__main__":
         "--page", dest="page", help="page URL or ID", required=True, type=str
  )
     parser.add_argument("--token", dest="token", help="token_v2", type=str)
+ args = parser.parse_args()
+
+    token = args.token
+    if not token:
+        # if you don't want your terminal to be filled with messy token, then input your token_v2 at "NOTION_TOKEN"
