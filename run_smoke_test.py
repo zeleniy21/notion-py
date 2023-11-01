1ff729aaa522428977d130661aa5125f24b6dcc1
Vladislav <137306980+zeleniy21@users.noreply.github.com>
2023-11-01 16:24:28 +0200

    Update run_smoke_test.py

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     token = args.token
  if not token:
         # if you don't want your terminal to be filled with messy token, then input your token_v2 at "NOTION_TOKEN"
+ token = os.environ.get("NOTION_TOKEN")
+    if not token:
