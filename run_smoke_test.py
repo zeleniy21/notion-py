d8346a8baf5772e00335e20981a2f1cba3b02730
Vladislav <137306980+zeleniy21@users.noreply.github.com>
2023-11-22 14:56:03 +0200

    Update run_smoke_test.py

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -9,3 +9,4 @@ from notion.smoke_test import run_live_smoke_test
 
 if __name__ == "__main__":
     description = "Run notion-py client smoke tests"
+  parser = argparse.ArgumentParser(description=description)
